2195a2bd72fadefc9a03c04ec90ea6f7534c703e
Josh Wiegand <joshwiegand55@gmail.com>
2017-06-20 09:20:38 -0400

    removed all css libs, style after functionality

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import main from '@/components/main'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'main',
-      component: main
+      component: Main
     }
   ]
 })
