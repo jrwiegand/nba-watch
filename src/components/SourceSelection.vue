bceaa26f4fb70f2d0d6d4dbb0a0121b9b47ce282
Josh Wiegand <joshwiegand55@gmail.com>
2017-06-27 22:32:01 -0400

    * created home component * using example to create app * using the vue-router to pass properties * updated packages

---
+++ src/components/SourceSelection.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="sourceSelection">
+    <div class="jumbotron">
+      <h2><span class="glyphicon glyphicon-list-alt"></span> News List</h2>
+      <h4>Select News Source</h4>
+      <select class="form-control" v-on:change="sourceChanged">
+        <option v-bind:value="source.id" v-for="source in sources">{{source.name}}</option>
+      </select>
+      <div v-if="source">
+        <h6>{{source.description}}</h6>
+        <a v-bind:href="source.url" class="btn btn-primary" target="_blank">Go to {{source.name}} Website</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sourceSelection',
+  data () {
+    return {
+      sources: [],
+      source: ''
+    }
+  },
+  methods: {
+    sourceChanged: function (event) {
+      this.sources.forEach(function (source) {
+        if (source.id === event.target.value) {
+          this.source = source
+        }
+      })
+      this.$emit('sourceChanged', event.target.value)
+    }
+  },
+  created: function () {
+    this.$http.get('https://newsapi.org/v1/sources?language=en').then(response => {
+      this.sources = response.data.sources
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from '@/components/Main'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -8,8 +8,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'main',
-      component: Main
+      name: 'Home',
+      component: Home,
+      props: true
     }
   ]
 })
