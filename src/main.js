0f372a05b135a358244607e48a6a9369a3645f78
Josh Wiegand <joshwiegand55@gmail.com>
2017-07-01 13:22:15 -0400

    added vuex and example store implementation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import app from './App'
+import vSelect from 'vue-select'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import app from './App'
+import store from './store'
 import router from './router'
-import vSelect from 'vue-select'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
@@ -14,6 +15,7 @@ Vue.component('v-select', vSelect)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<app/>',
   components: { app }
