808f66471f28f521b27bc99e1af6af99d9e6f6f3
Josh Wiegand <joshwiegand55@gmail.com>
2017-07-11 09:35:49 -0400

    updating the state store

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 // root state object.
 // each Vuex instance is just a single state tree.
 const state = {
-  count: 0
+  selectedSource: ''
 }
 
 // mutations are operations that actually mutates the state.
@@ -15,37 +15,18 @@ const state = {
 // mutations must be synchronous and can be recorded by plugins
 // for debugging purposes.
 const mutations = {
-  increment (state) {
-    state.count++
-  },
-  decrement (state) {
-    state.count--
+  setSelectedSource (state, selectedSource) {
+    state.selectedSource = selectedSource
   }
 }
 
 // actions are functions that causes side effects and can involve
 // asynchronous operations.
 const actions = {
-  increment: ({ commit }) => commit('increment'),
-  decrement: ({ commit }) => commit('decrement'),
-  incrementIfOdd ({ commit, state }) {
-    if ((state.count + 1) % 2 === 0) {
-      commit('increment')
-    }
-  },
-  incrementAsync ({ commit }) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        commit('increment')
-        resolve()
-      }, 1000)
-    })
-  }
 }
 
 // getters are functions
 const getters = {
-  evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd'
 }
 
 // A Vuex instance is created by combining the state, mutations, actions,
