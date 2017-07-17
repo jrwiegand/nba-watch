9f671ef993dfa3da6e73f13e22db55745a82a038
Josh Wiegand <joshwiegand55@gmail.com>
2017-07-16 20:04:32 -0400

    updated more store attributes, learning about vuex

--- src/store.js
+++ src/store.js
@@ -3,37 +3,21 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-// root state object.
-// each Vuex instance is just a single state tree.
-const state = {
-  selectedSource: ''
-}
-
-// mutations are operations that actually mutates the state.
-// each mutation handler gets the entire state tree as the
-// first argument, followed by additional payload arguments.
-// mutations must be synchronous and can be recorded by plugins
-// for debugging purposes.
-const mutations = {
-  setSelectedSource (state, selectedSource) {
-    state.selectedSource = selectedSource
-  }
-}
-
-// actions are functions that causes side effects and can involve
-// asynchronous operations.
-const actions = {
-}
-
-// getters are functions
-const getters = {
-}
-
-// A Vuex instance is created by combining the state, mutations, actions,
-// and getters.
 export default new Vuex.Store({
-  state,
-  getters,
-  actions,
-  mutations
+  state: {
+    selectedSource: ''
+  },
+  getters: {
+    getSelectedSource: state => {
+      return state.selectedSource
+    }
+  },
+  actions: {
+
+  },
+  mutations: {
+    setSelectedSource (state, selectedSource) {
+      state.selectedSource = selectedSource
+    }
+  }
 })
